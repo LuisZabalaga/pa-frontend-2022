8eb0241e218d80412e10be9bd792310c27db8810
LuisZabalaga <luchitozabalaga@gmail.com>
2023-04-22 06:00:09 -0500

    Cambios en la url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,3 @@ export const environment = {
   urlWs: 'wss://pa-backend-2022.vercel.app/'
 };
 
-// export const environment = {
-//   production: true,
-//   url: 'https://pa-backend-2022.herokuapp.com/',
-//   urlWs: 'wss://pa-backend-2022.herokuapp.com/'
-// };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,17 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  url: 'http://localhost:3001/',
-  urlWs: 'ws://localhost:3001/'
-};
-
 // export const environment = {
-//   production: true,
-//   url: 'https://pa-backend-2022.herokuapp.com/',
-//   urlWs: 'wss://pa-backend-2022.herokuapp.com/'
+//   production: false,
+//   url: 'http://localhost:3001/',
+//   urlWs: 'ws://localhost:3001/'
 // };
+
+export const environment = {
+  production: true,
+  url: 'https://pa-backend-2022.vercel.app/',
+  urlWs: 'wss://pa-backend-2022.vercel.app/'
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
